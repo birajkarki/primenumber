976de4f5c4898a24d4993a8a4e9fae24c1aaf006
Biraj Karki <65703910+birajkarki@users.noreply.github.com>
2020-09-30 23:43:43 +0545

    Update primenum.py

--- primenum.py
+++ primenum.py
@@ -1,6 +1,13 @@
-for num in range (2,1001):
+#Prints all prime number which is given by user
+num1 =int(input("Enter your beginning number \n"))
+num2 =int(input("Enter your ending number \n"))
+
+print(f"Your prime numbers from {num1} to {num2} is:")
+ # note that in for num in range(num1 appears but num2 doesnot so num2+1 while giving end number)
+for num in range (num1,num2):
     for i in range(2, num):
         if num %i ==0:
             break
     else:
         print(num)    
+ 
